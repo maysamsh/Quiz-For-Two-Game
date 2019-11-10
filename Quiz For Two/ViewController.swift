eb8842b02d6d1b33e8b9a40d631e18ac2f210a02
Bartek Bugajski <bartekbugajski@hotmail.com>
2019-11-10 20:49:58 +0100

    Initial Commit

---
+++ Quiz For Two/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz For Two
+//
+//  Created by Bartek Bugajski on 10/11/2019.
+//  Copyright © 2019 BB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
